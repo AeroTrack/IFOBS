b0053098eb15feb6d67150e75c48cc3cf5f78493
BowieGian <43588714+BowieGian@users.noreply.github.com>
2023-11-22 02:06:31 -0800

    Add new ballistic calculations

--- ballistics.c
+++ ballistics.c
@@ -3,10 +3,22 @@
 / ---------------------------------------------------------------------------- /
 /	Mint Luc
 /	Bowie Gian
+/	Hong Shi
 /	Created: 2023-06-30
-/	Modified: 2023-11-19
+/	Modified: 2023-11-16
 /
 /	This file contains the functions that calculates ballistic trajectory.
+/
+/	coordinate system
+/
+/	z yaw	^
+/			|
+/			|
+/			|_______> y cant
+/			/
+/		   /
+/		  /  x elevation
+/		 v
 / ----------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------*/
@@ -21,133 +33,187 @@
 /*--------------------------------------------------------------*/
 
 const double gravity = 9.8;
+const double v_muzzle = 390;  // Initial velocity in m/s
+const double elev_bias_rad = 0.012; // angle between muzzle and red dot
+// v_muzzle and elev_bias_rad will need to be tweaked during testing.
 
 /*--------------------------------------------------------------*/
 /*  Static Function Implemetations								*/
 /*--------------------------------------------------------------*/
 
-static double calculateTerminalVelocity(double ballistic_coefficient_metric, double density) {
-	double terminalVelocity = sqrt((2 * gravity * ballistic_coefficient_metric) / (density));
-	return terminalVelocity;
+// initial velocity components
+struct Vector {
+	double x, y, z;
+};
+
+static struct Vector rotate_vector(double cant_rad, double elev_rad, double r)
+{
+	struct Vector result;
+
+	// positive x is left
+	// positive y is forward (to target)
+	// positive z is up (to sky)
+
+	result.x = r * sin(cant_rad) * sin(elev_rad);
+	result.y = r * cos(elev_rad);
+	result.z = r * cos(cant_rad) * sin(elev_rad);
+
+	return result;
+
 }
 
-// static double calculateTerminalVelocity(double drag, double density, double area, double mass)
-// {
-// 	// Calculate terminal velocity
-// 	double terminalVelocity = sqrt((2 * mass * gravity) / (drag * density * area));
-// 	return terminalVelocity;
-// }
+static double calculateTime(double v0, double target_dist) {
+	// description: calculate the time of flight (eq 184)
+	// input :
+	//			v0 - inital velocity
+	//			target_dist - distance to the target measured by range finder
+	//
+	// output:
+	//			t  - expected time of flight
 
-static double calculateTime(double v0, double vt, double elev_rad, double x) {
-	double t = -(vt * log(1 - (x * gravity) / (v0 * vt * cos(elev_rad)))) / gravity;
+	double t = target_dist/(v0); // Calculate time of flight
 	return t;
 }
 
-// static double calculateNormalTimeOfFlight(double phi, double x, double v0)
-// {
-// 	double eleAngle= phi * M_PI / 180.0; // Convert angle to radians
-// 	double t = x/ (v0 * cos(eleAngle)); // Calculate time of flight
-// 	return t;
-// }
 
-// Function to calculate the height given the time of flight, initial velocity, and elevation angle
-// static double calculateNormalHeight(double t, double v0, double phi)
-// {
-// 	double eleAngle = phi * M_PI / 180.0; // Convert angle to radians
-// 	double y = v0 * t * sin(eleAngle) - (0.5 * 9.8 * pow(t, 2)); // Calculate height
-// 	return y;
-// }
 
-static double calculateTargetHeight(double h0, double x, double v0, double vt, double elev_red, double t)
+static double calculateTargetDisplacement(double v0, double g, double t)
 {
-	// Calculate the height of the target
-	double target_height = h0 + (vt / gravity) * (v0 * sin(elev_red) + vt) * (1 - exp(-gravity * t / vt)) - vt * t;
+	// description: Calculate the height of the target (e
+	// input: 
+	//			v0 - inital velocity,
+	//			t - time of bullet flight
+	//			g - gravity
+	// output:
+	//			target height - expected bullet drop / expected bullet displacement 
+	//
+
+
+	double target_height = (v0 * t) - (0.5 * g * pow(t, 2));
 
 	return target_height;
 }
 
-static double calculateMaxHeight(double v0, double vt, double elev_rad)
-{
-	// Calculate the time at which y is maximized (when the vertical velocity becomes zero)
-	double t_max_height = (vt * sin(elev_rad)) / gravity;
-
-	// Calculate the maximum height
-	double max_height = (vt / gravity) * (v0 * sin(elev_rad) + vt) * (1 - exp(-gravity * t_max_height / vt)) - (vt * t_max_height);
-
-	return max_height;
-}
 
 //  25m   0mm
 //  50m  50mm
 // 100m 250mm
-static double calculateDrop(double distance_m, double elev_deg)
+static struct Vector calculateDrop(double distance_m, double elev_rad, double cant_rad)
 {
-	double h0 = 0.0;  // Initial launch height in meters
-	double v0 = 430;  // Initial velocity in m/s
-	double elev_rad = elev_deg * M_PI / 180.0;  // Launch angle in degrees
-	double density = 1.225;
-	double ballistic_coefficient = 0.143;
-	double ballistic_coefficient_metric = ballistic_coefficient * 703.1;
 
-	double xDistance_m = distance_m * cos(elev_rad);
-	double yAimHeight = distance_m * sin(elev_rad);
+	struct Vector v0 = rotate_vector(cant_rad, elev_rad + elev_bias_rad, v_muzzle);
+	struct Vector aimHeight = rotate_vector(cant_rad, elev_rad, distance_m);
+	struct Vector offset; // calculation results is placed in offset vector
 
-	double vt = calculateTerminalVelocity(ballistic_coefficient_metric, density);
-	// double vt = calculateTerminalVelocity(0.08,0.37,0.01,0.01);
-	// printf("Terminal velocity: %.2f v/m\n", vt);
-	double t = calculateTime(v0, vt, elev_rad, xDistance_m);
-	// printf("Time: %.2f \n", t);
 
-	double target_height = calculateTargetHeight(h0, xDistance_m, v0, vt, elev_rad, t);
-	double max_height = calculateMaxHeight(v0, vt, elev_rad);	
-	double yDrop = target_height - yAimHeight;
 
-	printf("Drop: %.3f meters\n", yDrop);
-	// printf("Max height of the target: %.2f meters\n", max_height);
+	double t = calculateTime(v0.y, aimHeight.y); // when range finder is unlocked distance is range finder reading projected onto y-axis
+	//double t = calculateTime(v0.y, distance_m); // when range finder is locked distance is just range finder reading
 
-	return yDrop;
+	offset.y = 0; // theres only a LR bullet displacement and Up Down bullet displacement
+
+	offset.z = calculateTargetDisplacement(v0.z, gravity, t) - aimHeight.z;
+
+	offset.x = calculateTargetDisplacement(v0.x, 0, t) - aimHeight.x;
+	
+
+	//printf("Drop: %.3f meters, right: %.3f meters\n", offset.z, offset.x);
+
+	return offset;
 }
 
-// Converts polar (r, theta) to (x, y) output to the pointers
-static void polarTocartesian(double r, double theta_deg, double *x, double *y)
+static struct Vector proj2screen(double cant_rad, double elevation_rad, double x, double z)
 {
-	double theta_rad = theta_deg * M_PI / 180.0;
+	// description: takes the caculated bullet drop and LR displacement and project it onto the screen plane
+	// input:
+	//			cant_rad
+	//			elevation_rad
+	//			z vertical bullet drop , drop is negative
+	//			x LR displacement, right is positive 
+	struct Vector result;
 
-	*x = r * cos(theta_rad);
-	*y = r * sin(theta_rad);
+
+
+	// drop is lengthed based on screen angle (elevation)
+	z = z / cos(elevation_rad);
+
+	// rotate target displacement base on screen
+	result.x = x * cos(cant_rad) + z * sin(cant_rad);
+	result.z = -x * sin(cant_rad) + z * cos(cant_rad);
+	result.y = 0; // y is not used in the screen vector
+	return result;
+
 }
-
 /*--------------------------------------------------------------*/
 /*  Function Implemetations										*/
 /*--------------------------------------------------------------*/
 
-void Ballistics_calculatePixelOffset(double distance_m, double elev_deg,
-		double cant_deg, int *xOffset, int *yOffset)
+void Ballistics_calculatePixelOffset(double distance_m, double elev_deg, double cant_deg, int *xOffset, int *zOffset)
 {
 	const double pixelWidth = 0.000254;
-	const double xEyeToOptic = .05;
-	const double yHeightOverBore = .06;
+	const double EyeToOptic = .05; 
+	const double HeightOverBore = .06;
 
-	double drop_m = calculateDrop(distance_m, elev_deg);
+	double elev_rad = elev_deg * M_PI / 180.0;  // Launch angle in degrees
+	double cant_rad = cant_deg * M_PI / 180.0;
+
+	struct Vector offset = calculateDrop(distance_m, elev_rad, cant_rad);
+
+	double yTotal = distance_m + EyeToOptic;
+	offset.z = offset.z - HeightOverBore;
+
+	// convert drop distance to offset distance at eye to optic length
+	offset.z = offset.z / yTotal * EyeToOptic;
+	offset.x = offset.x / yTotal * EyeToOptic ;
 	
-	double yOffset_m = 0;
-	double zOffset_m = 0;
-	polarTocartesian(drop_m, (cant_deg + 90.0), &zOffset_m, &yOffset_m);
+	// project target drop (and LR displacement) onto the screen plane
+	struct Vector screen = proj2screen(cant_rad, elev_rad, offset.x,offset.z);
 
-	double xTotal = distance_m + xEyeToOptic;
-	double yTotal = yOffset_m - yHeightOverBore;
-	double zTotal = zOffset_m;
+	// convert m to pixels	
 
-	double yScreen = yTotal / xTotal * xEyeToOptic;
-	*yOffset = (int)round(yScreen / pixelWidth);
+	*zOffset = (int)round(screen.z / pixelWidth);
 
-	double zScreen = zTotal / xTotal * xEyeToOptic;
-	*xOffset = (int)round(zScreen / pixelWidth);
+	*xOffset = (int)round(screen.x / pixelWidth);
 
-	//printf("%lf %lf %lf %d\r\n", xTotal, yTotal, yScreen, yPixelOffset);
-	
-	double yAngle = atan2(yOffset_m, distance_m);
-	double zAngle = atan2(zOffset_m, distance_m);
-
-	printf("X Offset: %7.3lf Y Offset: %7.3lf\r\n", zAngle, yAngle);
+	return;
 }
+
+
+
+// // for testing 
+// int main() {
+// 	double distance_m = 100;
+// 	double elev_deg = 0; // up is positive
+// 	double cant_deg = 0; // right is positive
+// 	int xOffset = 0;
+// 	int zOffset = 0;
+
+// 	// test parameters: distance = 100m, 
+// 	// key: d = ~0.5m drop due to gravity or 1px, os = off screen 
+// 	// test cases: cant    elev   | drop	    right     screen drop 		sreen right
+// 	//				0		0	  |	-d          0            -d                  0
+// 	//				0       45    | -d          0            -d					 0
+// 	// 			    45		0     | -d          0          -0.707d 		     -0.707d
+// 	//				90      0     | -d          0             0			        -d
+// 	//              90     45     | -d         100           -os                -os
+// 	//              45     45     | -d          0            -d                  os
+
+
+// 	// Ballistics_calculatePixelOffset(distance_m, elev_deg, cant_deg, &xOffset, &zOffset);
+// 	// printf("cant Deg: %0.1f right: %d px, up: %d px \n", cant_deg, xOffset, zOffset);
+
+
+// 	/*
+// 	for (int i = 0; i<90; i+=1){
+// 		cant_deg = (double)i;
+// 		Ballistics_calculatePixelOffset(distance_m, elev_deg, cant_deg, &xOffset, &zOffset);
+// 		//printf("cant Deg: %f Drop: %d px, right: %d px \n", cant_deg, zOffset, xOffset);
+// 		printf("%.1f, %d, %d\n", cant_deg, xOffset, zOffset);
+
+// 	} 
+// 	*/
+
+
+//     return 0; 
+
+// }  
--- ballistics.h
+++ ballistics.h
@@ -3,17 +3,51 @@
 / ---------------------------------------------------------------------------- /
 /	Mint Luc
 /	Bowie Gian
+/	Hong Shi
 /	Created: 2023-06-30
-/	Modified: 2023-07-28
+/	Modified: 2023-11-17
 /
-/	This file contains the function declarations for the ballistics
-/	calculations.
+/	This file contains the function declarations for the ballistics calculations.
+/
+/
+/	coordinate system
+/
+/	z yaw	^
+/			|
+/			|
+/ 			|_______> y cant
+/			/
+/	 	   /
+/		  /  x elevation
+/		 v
 / ----------------------------------------------------------------------------*/
+
 #ifndef BALLISTICS_H
 #define BALLISTICS_H
 
-// Calculates the bullet drop and returns the offsets to xOffset and yOffset
-void Ballistics_calculatePixelOffset(double distance_m, double elev_deg,
-		double cant_deg, int *xOffset, int *yOffset);
+// Description: Calculates the bullet drop and returns the offsets as a pixel value to xOffset and zOffset 
+//				screen pixel offset is a positive or negative int with respect to a (0,0) center screen
+// Input :
+//			distance_m:		distance from optic to target measured by lazer range finder
+//							this Value should be locked via the range finder lock button
+//
+//			elev_deg:		rifle elevation with: 0deg  -> aimed flat down range
+//												 90deg -> aimed to the sky
+//												-90deg -> aimed at the g 
+//
+//			cant_deg:		rifle cant with:  0deg -> aimed flat down range
+//											 90deg -> rifle rotated 90deg to its right
+//											-90deg -> rifle rotated 90deg to its left
+//							                        
+//			xOffset:		+1 -> 1 pixel to the right
+//							-1 -> 1 pixel to the left
+//
+//			zOffset:		+1 -> 1 pixel upwards
+//							+1 -> 1 pixel downwards
+//
+// Output :
+//			None
+
+void Ballistics_calculatePixelOffset(double distance_m, double elev_deg, double cant_deg, int *xOffset, int *zOffset);
 
 #endif
